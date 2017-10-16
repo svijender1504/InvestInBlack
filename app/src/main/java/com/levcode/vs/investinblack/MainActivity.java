69dba6f5ba08928401fd338df52870a780a5e6b4
Vijender Singh <vijendersingh@Vijenders-MacBook-Pro.local>
2017-10-16 12:46:29 -0400

    First commit

---
+++ app/src/main/java/com/levcode/vs/investinblack/MainActivity.java
@@ -0,0 +1,25 @@
+package com.levcode.vs.investinblack;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+
+        browser.loadUrl("http://investinblack.co");
+//        Uri uri = Uri.parse("https://www.example.com");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        startActivity(intent);
+
+
+    }
+}
